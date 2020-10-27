fd84455167d5725dffa895b72e009af612f26b65
idrewdragons <andrewcarey1111@gmail.com>
2020-10-26 20:32:49 -0400

    day 3 finished

---
+++ index.js
@@ -0,0 +1,64 @@
+require('dotenv').config(); // this will read the .env file, if it exists
+
+const { PORT = 3000, WEATHER_KEY } = process.env;
+
+const express = require('express');
+const server = express();
+
+const morgan = require('morgan');
+server.use(morgan('dev'));
+
+server.use(express.static('public'));
+
+const bodyParser = require('body-parser');
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: false }));
+
+const cowsay = require('cowsay');
+const Quote = require('inspirational-quotes')
+
+server.get('/cowspiration', (req, res) => {
+  const { text, author } = Quote.getQuote();
+
+  const cow = cowsay.say({
+    text: `${ text }\n\n- ${ author }`,
+    W: 80,
+  });
+
+  res.send({ cow });
+});
+
+const axios = require('axios');
+
+server.post('/job-search', async (req, res) => {
+  try {
+    const { description, fulltime } = req.body;
+
+    const URL = `https://jobs.github.com/positions.json?${
+      description ? `description=${ description }&` : ''
+    }${
+      fulltime ? 'fulltime=on' : ''
+    }`;
+
+    const { data } = await axios.get(URL);
+    res.send({ results: data });
+  } catch (error) {
+    res.send({ error })
+  }
+});
+
+server.get('/weather', async (req, res) => {
+  try {
+    const { lat, lon } = req.query;
+    const URL = `https://api.openweathermap.org/data/2.5/onecall?lat=${ lat }&lon=${ lon }&appid=${ WEATHER_KEY }`;
+
+    const { data } = await axios.get(URL);
+    res.send({ results: data });
+  } catch (error) {
+    res.send({ error });
+  }
+});
+
+server.listen(3000, () => {
+  console.log('I am listening...');
+});
---
+++ public/app.js
@@ -0,0 +1,109 @@
+$('#job-search').on('submit', async (event) => {
+  event.preventDefault();
+
+  try {
+    const response = await fetch('/job-search', {
+      method: "POST",
+      body: JSON.stringify({
+        description: $('#description').val(),
+        fulltime: $('#fulltime').is(':checked')
+      })
+    });
+  
+    const { results } = await response.json();
+    
+    renderJobList(results);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+function renderJobList(jobList) {
+  $('#cow').remove();
+  $('#results').empty();
+
+  jobList.forEach(job => {
+    $('#results').append(renderJob(job));
+  });
+}
+
+function renderJob({
+  company,
+  company_logo,
+  company_url,
+  location,
+  title,
+  type,
+  url,
+  description,
+  how_to_apply
+}) {
+  return $(`
+    <div class="listing">
+      <header>
+        <div>
+          <h2><a href="${ company_url }">${ company }</a></h2>
+          <h3>${ location }</h3>
+          <h3>${ type }, ${ title }</h3>
+        </div>  
+        <img src="${ company_logo }" />
+      </header>
+      <main>
+        ${ description }
+      </main>
+      <footer>
+        ${ how_to_apply }
+      </footer>
+    </div>
+  `);
+}
+
+async function fetchQuote() {
+  try {
+    const response = await fetch('/cowspiration');
+    const { cow } = await response.json();
+  
+    $('#app > main').append($(`<pre id="cow">${ cow }</pre>`));
+  } catch (error) {
+    console.error(error);
+  } 
+}
+
+fetchQuote();
+
+async function fetchWeather() {
+  if (!navigator.geolocation) {
+    $('#weather').append($('<h3>Weather not available on this browser</h3>'))
+  }
+
+  navigator.geolocation.getCurrentPosition(async (position) => {
+    const { coords: { latitude, longitude } } = position;
+
+    const response = await fetch(`/weather?lat=${ latitude }&lon=${ longitude }`);
+    const { results } = await response.json();
+
+    if (results.daily) {
+      $('#weather').empty();
+
+      results.daily.forEach(day => {
+        const { weather: [{ icon }] } = day;
+
+        $('#weather').append($(`
+          <img src="http://openweathermap.org/img/wn/${ icon }@2x.png" />
+        `));
+      });
+    }
+  }, async (error) => {
+    const result = await navigator.permissions.query({ name: 'geolocation' });
+    if (result.state == "denied") {
+      $('#weather').html(
+        $(`<div>
+            <h3>You have previously denied access to location services.</h3>
+            <h4>If you wish to see your forecast, update your settings and refresh.</h4>
+          </div>`)
+      )
+    }
+  });
+}
+
+fetchWeather();
